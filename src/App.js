b937d12632c9d4ac8364922b66261c4cae287170
kyohei uno <waizeru00@gmail.com>
2021-09-02 13:35:05 +0900

    初期描画の修正

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
 
-class App extends Comment {
+class App extends Component {
   render() {
     return (
       <div>
