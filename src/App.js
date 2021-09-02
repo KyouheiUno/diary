887013ea68603584a2e882cc90522f4b2b6f8d3a
kyohei uno <waizeru00@gmail.com>
2021-09-02 13:47:38 +0900

    Appコンポーネントの修正

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { Component} from 'react';
 import './App.css';
 
 class App extends Component {
+
+  //属性を定義
+  title = "初期値のタイトルです";
+  message = "初期値のメッセージです";
+
+  //コンストラクターを定義
+  constructor(props) {
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
+
   render() {
     return (
       <div>
-        <h1>React</h1>
-        <p>This is sample component.</p>
-        <p>これはサンプルのコンポーネントです。</p>
+        <h1>{this.title}</h1>
+        <p>{this.message}</p>
       </div>
     );
   }
