45b294b87c36003591b5916c8c979ec990aac048
kyohei uno <waizeru00@gmail.com>
2021-09-13 05:23:33 +0900

    コンポーネントとマップ

--- src/App.js
+++ src/App.js
@@ -3,40 +3,88 @@ import React, { Component} from 'react';
 import Member from './Member';
 import './App.css';
 
+//コンポーネントを定義
 class App extends Component {
 
+  data = [
+    "これはリストのサンプルです",
+    "配列をリストに変換します",
+    "This is list sample"
+  ]
+
   msgStyle = {
-    fontSize: "24px",
+    fontSize: "20pt",
     color: "#900",
     margin: "20px 0px",
-    padding: "5px",
-    borderBottom: "2px solid #900"
-  }
-  btnStyle = {
-    fontSize: "20px",
-    padding: "0px 10px"
-  }
-  //コンストラクターを定義
-  constructor(props) {
-    super();
-    this.state = {
-      msg: "Hello"
-    };
-    //関数を定義する
-    let timer = setInterval(()=>{
-      this.setState((state)=>({
-        msg: state.msg + "!"
-      }));
-    }, 5000);
+    padding: "5px"
   }
 
-  render() {
+  //コンストラクターを定義
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: this.data
+    };
+  }
+
+  render(){
     return (
       <div>
         <h1>React</h1>
-        <p style={this.msgStyle}>{this.state.msg}</p>
-        <p style={this.msgStyle}>{this.props.msg}</p>
+        <h2 style={this.msgStyle}>show list.</h2>
+        <List title="サンプル・リスト" data={this.data} />
       </div>
+    )
+  }
+}
+
+//リストコンポーネントを新たに定義
+class List extends Component {
+  number = 1;
+
+  title = {
+    fontSize: "20pt",
+    fontWeight: "bold",
+    color: "blue"
+  };
+
+  render(){
+    let data = this.props.data
+    return (
+      <div>
+        <p style={this.title}>{this.props.title}</p>
+        <ul>
+          {data.map((item) =>
+            <Item number={this.number++} value={item} key={this.number} /> )
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+//アイテムコンポーネントを新たに定義
+class Item extends Component {
+  //CSSを定義
+  li = {
+    listStyleType: "square",
+    fontSize: "16pt",
+    color: "#06",
+    margin: "0px",
+    padding: "red"
+  }
+
+  num = {
+    fontWeight: "bold",
+    color: "red"
+  }
+
+  render(){
+    return (
+      <li style={this.li}>
+        <span style={this.num}>[{this.props.number}]</span>
+          {this.props.value}
+      </li>
     );
   }
 }
